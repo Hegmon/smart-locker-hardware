b5ddc39eefff8959cddde33d0e49882da3219e7c
Hassaan_98 <hassaanxd8@gmail.com>
2026-05-02 02:29:36 +0500

    Update utils.py

--- app/hardware_agent/provisioning/ble/utils.py
+++ app/hardware_agent/provisioning/ble/utils.py
@@ -2,10 +2,10 @@ def get_device_name():
     try:
         with open("/proc/cpuinfo") as f:
             for line in f:
-                if line.startwith("Serial"):
-                   serial=line.split(":")[1].strip()[-6:]
-                   return f"SmartLocker-{serial}"
-    except:
+                if line.startswith("Serial"):
+                    serial = line.split(":")[1].strip()[-6:]
+                    return f"SmartLocker-{serial}"
+    except Exception:
         pass
-    
+
     return "SmartLocker-UNKNOWN"
