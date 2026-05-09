e5733dc0a17ad8f87119a44df0532ccd84c95b7b
Hassaan_98 <hassaanxd8@gmail.com>
2026-05-09 12:37:39 +0500

    Create config_loader.py

---
+++ app/streaming_agent/config_loader.py
@@ -0,0 +1,23 @@
+import json 
+from pathlib import Path
+
+BASE_DIR=Path(__file__).parent.parent
+
+CONFIG_FILE=BASE_DIR / "config"
+
+BACKEND_CONFIG_FILE=CONFIG_FILE / "backend_config.json"
+
+def load_backend_device_config():
+    if not BACKEND_CONFIG_FILE.exists():
+        raise FileNotFoundError(f"Backend config file not found:{BACKEND_CONFIG_FILE}")
+    with open(BACKEND_CONFIG_FILE,"r") as file:
+        config=json.load(file)
+
+    return config
+def get_device_id():
+    config=load_backend_device_config()
+    device_id=config.get("device_id")
+    if not device_id:
+        raise ValueError("Device ID not found in backend config")
+    print("Loaded device ID:",device_id)
+    return device_id
