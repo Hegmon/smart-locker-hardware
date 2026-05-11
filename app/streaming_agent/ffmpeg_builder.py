7c5613bb200bf906de21599273e7f1d9be002d24
Hassaan_98 <hassaanxd8@gmail.com>
2026-05-11 16:17:27 +0500

    Update ffmpeg_builder.py

--- app/streaming_agent/ffmpeg_builder.py
+++ app/streaming_agent/ffmpeg_builder.py
@@ -13,28 +13,43 @@ def build_rtsp_url(camera_role):
 def build_ffmpeg_command(video_device, camera_role):
     rtsp_url = build_rtsp_url(camera_role)
     return  [
-        "ffmpeg",
-        "-loglevel", "warning",
-        "-fflags", "nobuffer",
-        "-flags", "low_delay",
-        "-f", "v4l2",
-        "-input_format", "mjpeg",
-        "-video_size", "1280x720",
-        "-framerate", "20",
-        "-i", video_device,
-        "-an",
-        "-c:v", "libx264",
-        "-preset", "ultrafast",
-        "-tune", "zerolatency",
-        "-pix_fmt", "yuv420p",
-        "-profile:v", "baseline",
-        "-b:v", "700k",
-        "-maxrate", "700k",
-        "-bufsize", "350k",
-        "-g", "20",
-        "-keyint_min", "20",
-        "-sc_threshold", "0",
-        "-rtsp_transport", "tcp",
-        "-f", "rtsp",
-        rtsp_url,
-    ]
+    "ffmpeg",
+    "-loglevel", "warning",
+
+    # Low latency
+    "-fflags", "nobuffer",
+    "-flags", "low_delay",
+
+    # Camera input
+    "-f", "v4l2",
+    "-input_format", "mjpeg",
+    "-video_size", "1280x720",
+    "-framerate", "30",
+    "-i", video_device,
+
+    "-an",
+
+    # Encoder
+    "-c:v", "libx264",
+    "-preset", "veryfast",          # better quality than ultrafast
+    "-tune", "zerolatency",
+
+    "-pix_fmt", "yuv420p",
+    "-profile:v", "main",
+
+    # Quality settings
+    "-b:v", "2500k",
+    "-maxrate", "3000k",
+    "-bufsize", "6000k",
+
+    # GOP
+    "-g", "30",
+    "-keyint_min", "30",
+    "-sc_threshold", "0",
+    "-bf", "0",
+
+    # RTSP output
+    "-rtsp_transport", "tcp",
+    "-f", "rtsp",
+    rtsp_url,
+]
