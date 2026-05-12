fa1d9cdec6a82e908b8ce5600b3def3ed6c141f6
Hassaan_98 <hassaanxd8@gmail.com>
2026-05-12 10:25:36 +0500

    Update service.py

--- app/hardware_agent/provisioning/ble/service.py
+++ app/hardware_agent/provisioning/ble/service.py
@@ -1,27 +1,47 @@
+from __future__ import annotations
+
+import dbus
+import dbus.service
+
 from app.hardware_agent.provisioning.ble.characteristic import (
     CommandCharacteristic,
     ResponseCharacteristic,
 )
 
+GATT_SERVICE_IFACE = "org.bluez.GattService1"
+DBUS_PROP_IFACE = "org.freedesktop.DBus.Properties"
 SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
 
 
-class SmartLockerService:
+class SmartLockerService(dbus.service.Object):
     def __init__(self, bus, handler):
         self.path = "/org/bluez/example/service0"
         self.bus = bus
+        super().__init__(bus, self.path)
 
         self.command_char = CommandCharacteristic(bus, self, handler)
         self.response_char = ResponseCharacteristic(bus, self)
 
     def get_properties(self):
         return {
-            "org.bluez.GattService1": {
-                "UUID": SERVICE_UUID,
-                "Primary": True,
-                "Characteristics": [
-                    self.command_char.path,
-                    self.response_char.path,
-                ],
+            GATT_SERVICE_IFACE: {
+                "UUID": dbus.String(SERVICE_UUID),
+                "Primary": dbus.Boolean(True),
+                "Characteristics": dbus.Array(
+                    [
+                        dbus.ObjectPath(self.command_char.path),
+                        dbus.ObjectPath(self.response_char.path),
+                    ],
+                    signature="o",
+                ),
             }
         }
+
+    @dbus.service.method(DBUS_PROP_IFACE, in_signature="s", out_signature="a{sv}")
+    def GetAll(self, interface):
+        if interface != GATT_SERVICE_IFACE:
+            raise dbus.exceptions.DBusException(
+                "org.freedesktop.DBus.Error.InvalidArgs",
+                "Invalid interface requested",
+            )
+        return self.get_properties()[GATT_SERVICE_IFACE]
