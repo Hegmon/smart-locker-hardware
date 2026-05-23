13f735e8b0542e9cca7e2f6fe41c0ad36acab8d0
Hassaan_98 <hassaanxd8@gmail.com>
2026-05-23 10:19:09 +0500

    Create __init__.py

---
+++ app/streaming_agent/config/__init__.py
@@ -0,0 +1,15 @@
+from app.streaming_agent.config.runtime import (
+    DetectionEventConfig,
+    PersonDetectionConfig,
+    RelayConfig,
+    StreamingAgentRuntimeConfig,
+    TamperDetectionConfig,
+)
+
+__all__ = [
+    "DetectionEventConfig",
+    "PersonDetectionConfig",
+    "RelayConfig",
+    "StreamingAgentRuntimeConfig",
+    "TamperDetectionConfig",
+]
